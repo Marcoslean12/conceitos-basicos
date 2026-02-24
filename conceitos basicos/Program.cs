4cbbb4b9c20935a2a51b6e848f8c797971fa92c1
Marcoslean12 <amaralmedeirosm@gmail.com>
2026-02-24 20:04:38 -0300

    verificando idade do usuário maior de 18

--- conceitos basicos/Program.cs
+++ conceitos basicos/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.ComponentModel.Design;
+
+int idade;
+idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite a sua idade: ");
+if (idade >= 18)
+{
+    Console.WriteLine("Você é o maior de idade!");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade!");
+}
