4fdd004d4beadd4445e855357a45a7ec4dd21f2a
Marcoslean12 <amaralmedeirosm@gmail.com>
2026-03-24 21:33:56 -0300

    Testes e Relacionamentos

--- ConceitosPOO/Conta.cs
+++ ConceitosPOO/Conta.cs
@@ -10,6 +10,20 @@ namespace ConceitosPOO
     {
         private long _numero;
         private decimal _saldo;
+        private Cliente _titular;
+        private Agencia _agencia;
+
+        public Agencia Agencia
+        {
+            get { return _agencia; }
+            set { _agencia = value; }
+        }
+
+        public Cliente Titular
+        {
+            get { return _titular; }
+            set { _titular = value; }
+        }
 
         public Conta(long numero, decimal saldo)
         {
@@ -45,6 +59,13 @@ namespace ConceitosPOO
             //_saldo = _saldo - valor;
             else
                 throw new ArgumentException("O valor tem que ser positivo");
+        }
+
+        public void Transferencia(Conta contaDestino, decimal valorTransferencia)
+        {
+            if (valorTransferencia <= 0) throw new ArgumentException("O valor a ser transferido deve ser superior a Zero!");
+            this.Saque(valorTransferencia);
+            contaDestino.Deposito(valorTransferencia);
         }
     }
 }
--- TesteConceitosPOO/TesteConta.cs
+++ TesteConceitosPOO/TesteConta.cs
@@ -29,25 +29,60 @@ namespace TesteConceitosPOO
         public void TestSaque()
         {
             //cenário
-            decimal saldoInicialS = 2000;
+            decimal saldoInicial = 2000;
             decimal valorSaque = 500;
-            decimal saldoEsperadoS = 1500;
-            Conta conta = new Conta(1111, saldoInicialS);
+            decimal saldoEsperado = 1500;
+            Conta conta = new Conta(1111, saldoInicial);
 
             //ação
             conta.Saque(valorSaque);
 
             //verificação
-            Assert.AreEqual(saldoEsperadoS, conta.Saldo);
+            Assert.AreEqual(saldoEsperado, conta.Saldo);
         }
         [TestMethod]
         public void TestTransferencia()
         {
             //cenário
+            decimal saldoInicialContaOrigem = 1000;
+            decimal saldoInicialContaDestino = 1000;
+            decimal valorTransferencia = 1000;
+            decimal saldoFinalContaOrigem = 0;
+            decimal saldoFinalContaDestino = 2000;
+            Conta contaOrigem, contaDestino;
+            contaOrigem = new Conta(1111, saldoInicialContaOrigem);
+            contaDestino = new Conta(2222, saldoInicialContaDestino);
 
             //ação
+            contaOrigem.Transferencia(contaDestino, valorTransferencia);
 
-           //verificação
+            //verificação
+            Assert.AreEqual(saldoFinalContaOrigem, contaOrigem.Saldo);
+            Assert.AreEqual(saldoFinalContaDestino, contaDestino.Saldo);
+            
+        }
+        [TestMethod]
+        public void TestDepositoMenorZero()
+        {
+            //cenario
+            decimal saldoInicial = 1000;
+            decimal valorDeposito =-500;
+            Conta conta = new Conta(1111, saldoInicial);
+
+            //ação e verificação
+            Assert.ThrowsException<ArgumentException>(() => conta.Deposito(valorDeposito));
+
+        }
+        [TestMethod]
+        public void TestSaqueMenorZero()
+        {
+            //cenario
+            decimal saldoInicial = 1000;
+            decimal valorSaque = -500;
+            Conta conta = new Conta(1111, saldoInicial);
+
+            //ação e varificação 
+            Assert.ThrowsException<ArgumentException>(() => conta.Saque(valorSaque));
 
         }
     }
